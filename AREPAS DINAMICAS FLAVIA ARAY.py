e25f14228742aaa439fe7b34f3ea31d5eb748aaf
flaviaaray <113453588+flaviaaray@users.noreply.github.com>
2025-01-13 01:11:28 -0400

    Add files via upload

---
+++ AREPAS DINAMICAS FLAVIA ARAY.py
@@ -0,0 +1,38 @@
+#AREPAS DINAMICAS FLAVIA ARAY
+
+#RECETA DE LAS AREPAS
+
+#Ingredientes
+#-1 tza agua
+#-1 tza harina PAN
+#-1 cdta sal
+#-1 cda aceite
+
+#Preparación
+#1.En un bol, se vierte el agua, la harina y la sal
+#2.Se mezcla hasta que quede uniforme
+#3.Se le da a la mezcla forma de disco
+#4.Se coloca con el aceite en un budare hasta dorar
+#5.Se voltea y repite
+#6.Se sirve en un plato
+
+#Considere las siguientes reglas de cocina en su programa:
+#-1 cda = 3 cdtas
+#-1 tza = 16 cda
+#-El 10% del volmen se evapora en el proceso de cocción
+
+#-------------------------------------------------------
+
+input("Cuanto de Harina PAN debe usar? ")
+input("Cuanto de agua debe usar? ")
+input("Cuanto de sal debe usar? ")
+input("Cuanto de aceite debe usar? ")
+
+print("En un bol, se vierte el agua, la harina y la sal")  
+print("Se mezcla hasta que quede uniforme")
+print("Se le da a la mezcla forma de disco")
+print("Se coloca con el aceite en un budare hasta dorar")
+print("Se voltea y repite")
+print("Se sirve en un plato")
+
+print("Si usa estas proporciones, vas a obtener 1 arepa con un volumen de 27,9 cdtas")
